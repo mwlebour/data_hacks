e2bf567bd80b350e9a3a04f95456418b84d1cedf
Matthew LeBourgeois <mlebourg@akamai.com>
2016-06-30 12:01:35 -0400

    Refactor such that boundaries represent each boundary on either side of each bucket.  There is now one more boundary than bucket.  This allows us to add a -inf and +inf boundary to autmatically track over and underflow.
    Added ability to suppress header and to show over/underflow.
    
    Short option for dot.

--- data_hacks/histogram.py
+++ data_hacks/histogram.py
@@ -31,6 +31,27 @@ import math
 from optparse import OptionParser
 from collections import namedtuple
 
+def first_bucket_size(k, n):
+    """Logarithmic buckets means, the size of bucket i+1 is twice
+    the size of bucket i.
+    For k+1 buckets whose sum is n, we have
+    (note, k+1 buckets, since 0 is counted as well):
+        \sum_{i=0}^{k} x*2^i   = n
+        x * \sum_{i=0}^{k} 2^i = n
+        x * (2^{k+1} - 1)      = n
+        x = n/(2^{k+1} - 1)
+    """
+    return n/(2**(k+1)-1)
+
+def log_steps(k, n):
+    "k logarithmic steps whose sum is n"
+    x = first_bucket_size(k-1, n)
+    sum = 0
+    for i in range(k):
+        sum += 2**i * x
+        yield sum
+
+
 
 class MVSD(object):
     "A class that calculates a running Mean / Variance / Standard Deviation"
@@ -128,136 +149,146 @@ def histogram(stream, options):
 
     stream yields Decimal()
     """
-    if not options.min or not options.max:
-        # glob the iterator here so we can do min/max on it
-        data = list(stream)
-    else:
-        data = stream
-    bucket_scale = 1
-
-    if options.min:
-        min_v = Decimal(options.min)
-    else:
-        min_v = min(data, key=lambda x: x.value)
-        min_v = min_v.value
-    if options.max:
-        max_v = Decimal(options.max)
-    else:
-        max_v = max(data, key=lambda x: x.value)
-        max_v = max_v.value
-
-    if not max_v > min_v:
-        raise ValueError('max must be > min. max:%s min:%s' % (max_v, min_v))
-    diff = max_v - min_v
 
     boundaries = []
     bucket_counts = []
     buckets = 0
+    bucket_scale = 1
 
+    # always track underflow
+    boundaries.append(Decimal("-Infinity"))
+
+    # calculate boundaries first
     if options.custbuckets:
+
+        # assume bucket list is each edge, i.e.,
+        # there is one less bucket than specified values
+        # e.g., 0,1,2,3,4 has 4 buckets (one at each comma)
         bound = options.custbuckets.split(',')
         bound_sort = sorted(map(Decimal, bound))
 
-        # if the last value is smaller than the maximum, replace it
-        if bound_sort[-1] < max_v:
-            bound_sort[-1] = max_v
-
         # iterate through the sorted list and append to boundaries
         for x in bound_sort:
-            if x >= min_v and x <= max_v:
-                boundaries.append(x)
-            elif x >= max_v:
-                boundaries.append(max_v)
-                break
+            boundaries.append(x)
 
-        # beware: the min_v is not included in the boundaries,
-        # so no need to do a -1!
-        bucket_counts = [0 for x in range(len(boundaries))]
-        buckets = len(boundaries)
-    elif options.logscale:
-        buckets = options.buckets and int(options.buckets) or 10
-        if buckets <= 0:
-            raise ValueError('# of buckets must be > 0')
-
-        def first_bucket_size(k, n):
-            """Logarithmic buckets means, the size of bucket i+1 is twice
-            the size of bucket i.
-            For k+1 buckets whose sum is n, we have
-            (note, k+1 buckets, since 0 is counted as well):
-                \sum_{i=0}^{k} x*2^i   = n
-                x * \sum_{i=0}^{k} 2^i = n
-                x * (2^{k+1} - 1)      = n
-                x = n/(2^{k+1} - 1)
-            """
-            return n/(2**(k+1)-1)
-
-        def log_steps(k, n):
-            "k logarithmic steps whose sum is n"
-            x = first_bucket_size(k-1, n)
-            sum = 0
-            for i in range(k):
-                sum += 2**i * x
-                yield sum
-        bucket_counts = [0 for x in range(buckets)]
-        for step in log_steps(buckets, diff):
-            boundaries.append(min_v + step)
     else:
-        buckets = options.buckets and int(options.buckets) or 10
+        # need to determin min, max and step
+
+        # glob the iterator here so we can do min/max on it
+        if not options.min or not options.max:
+            stream = list(stream)
+
+        min_v = ( Decimal(options.min) if options.min 
+                    else min(stream, key=lambda x: x.value).value)
+        max_v = ( Decimal(options.max) if options.max 
+                    else max(stream, key=lambda x: x.value).value)
+
+            # check to make sure all values aren't the same
+        if not options.min or not options.max:
+            if max_v <= min_v:
+                max_v = min_v + 1;
+
+
+        # should be fine if we entered if above, but if user specified
+        # need to make sure max is greater than min (though theoretically we
+        # could have a decreasing x-axis).
+        if not max_v > min_v:
+            raise ValueError('max must be > min. max:%s min:%s' % (max_v, min_v))
+
+        diff = max_v - min_v
+        buckets = 10 if not options.buckets else int(options.buckets)
+
         if buckets <= 0:
             raise ValueError('# of buckets must be > 0')
-        step = diff / buckets
-        bucket_counts = [0 for x in range(buckets)]
-        for x in range(buckets):
-            boundaries.append(min_v + (step * (x + 1)))
 
-    skipped = 0
-    samples = 0
+        # for nBuckets, we need nBuckets+1 boundaries
+        boundaries.append(min_v)
+
+        if options.logscale:
+            for step in log_steps(buckets, diff):
+                boundaries.append(min_v + step)
+        else:
+            step = diff / buckets
+            for x in range(buckets):
+                boundaries.append(min_v + (step * (x + 1)))
+
+    # always track overflow
+    boundaries.append(Decimal("Infinity"))
+
+    buckets = len(boundaries) - 1
+    bucket_counts = [0 for x in range(buckets)]
+
     mvsd = MVSD()
     accepted_data = []
-    for record in data:
-        samples += record.count
+    max_v = Decimal("-Infinity")
+    min_v = Decimal("Infinity")
+    samples = 0
+
+    # let's fill
+    for record in stream:
+
+        samples += 1
+
+        # track min,max value
+        if record.value > max_v:
+            max_v = record.value
+        if record.value > min_v:
+            min_v = record.value
+
         if options.mvsd:
             mvsd.add(record.value, record.count)
             accepted_data.append(record)
-        # find the bucket this goes in
-        if record.value < min_v or record.value > max_v:
-            skipped += record.count
-            continue
+
         for bucket_postion, boundary in enumerate(boundaries):
-            if record.value <= boundary:
-                bucket_counts[bucket_postion] += record.count
+            # boundaries are ordered so if rhs of bucket is greater than
+            # value, fill the bucket between current boundary and previous
+            # boundary
+            if boundary > record.value:
+                bucket_counts[bucket_postion-1] += record.count
                 break
+
 
     # auto-pick the hash scale
     if max(bucket_counts) > 75:
         bucket_scale = int(max(bucket_counts) / 75)
 
-    print("# NumSamples = %d; Min = %0.2f; Max = %0.2f" %
-          (samples, min_v, max_v))
-    if skipped:
-        print("# %d value%s outside of min/max" %
-              (skipped, skipped > 1 and 's' or ''))
-    if options.mvsd:
-        print("# Mean = %f; Variance = %f; SD = %f; Median %f" %
-              (mvsd.mean(), mvsd.var(), mvsd.sd(),
-               median(accepted_data, key=lambda x: x.value)))
-    print "# each " + options.dot + " represents a count of %d" % bucket_scale
-    bucket_min = min_v
-    bucket_max = min_v
+    nSkipped = 0
+    if not options.overunder:
+        nSkipped = bucket_counts[0] + bucket_counts[-1]
+
+    area = sum(bucket_counts) - nSkipped
+
+    # header
+    if not options.quiet:
+        print("# NumSamples = %d; Area = %0.2f; Min = %0.2f; Max = %0.2f" %
+              (samples,area,min_v,max_v))
+
+        if not options.overunder and nSkipped > 0:
+            print("# %d value%s outside of min/max" %
+                  (nSkipped, nSkipped > 1 and 's' or ''))
+
+        if options.mvsd:
+            print("# Mean = %f; Variance = %f; SD = %f; Median %f" %
+                  (mvsd.mean(), mvsd.var(), mvsd.sd(),
+                   median(accepted_data, key=lambda x: x.value)))
+
+        print "# each " + options.dot + " represents a count of %d" % bucket_scale
+
     percentage = ""
     format_string = options.format + ' - ' + options.format + ' [%6d]: %s%s'
+
     for bucket in range(buckets):
-        bucket_min = bucket_max
-        bucket_max = boundaries[bucket]
+        if not options.overunder and (bucket == 0 or bucket == buckets-1):
+            continue
+        bucket_min = boundaries[bucket]
+        bucket_max = boundaries[bucket+1]
         bucket_count = bucket_counts[bucket]
-        star_count = 0
-        if bucket_count:
-            star_count = bucket_count / bucket_scale
+        star_count = bucket_count / bucket_scale
         if options.percentage:
             percentage = " (%0.2f%%)" % (100 * Decimal(bucket_count) /
-                                         Decimal(samples))
-        print format_string % (bucket_min, bucket_max, bucket_count, options.dot *
-                               star_count, percentage)
+                                         Decimal(area))
+        print format_string % (bucket_min, bucket_max, bucket_count, 
+                                options.dot * star_count, percentage)
 
 
 if __name__ == "__main__":
@@ -270,9 +301,13 @@ if __name__ == "__main__":
                       default=False, action="store_true", help="Two column " +
                       "input format, space seperated with key<space>value")
     parser.add_option("-m", "--min", dest="min",
-                      help="minimum value for graph")
+                      help="minimum value for graph; not specifying both min"
+                      " and max disables streaming, all data will be loaded"
+                      " into memory before histogramming")
     parser.add_option("-x", "--max", dest="max",
-                      help="maximum value for graph")
+                      help="maximum value for graph; not specifying both min"
+                      " and max disables streaming, all data will be loaded"
+                      " into memory before histogramming")
     parser.add_option("-b", "--buckets", dest="buckets",
                       help="Number of buckets to use for the histogram")
     parser.add_option("-l", "--logscale", dest="logscale", default=False,
@@ -288,13 +323,25 @@ if __name__ == "__main__":
                       help="format for bucket numbers")
     parser.add_option("-p", "--percentage", dest="percentage", default=False,
                       action="store_true", help="List percentage for each bar")
-    parser.add_option("--dot", dest="dot", default='∎', help="Dot representation")
+    parser.add_option("-d","--dot", dest="dot", default='∎', help="Dot representation")
+    parser.add_option("-q","--quiet", dest="quiet", action="store_true", help="ssssh, no header")
+    parser.add_option("-o","--overunder", dest="overunder", action="store_true", 
+            help="Add overflow and underflow bins")
 
     (options, args) = parser.parse_args()
+
+    if (options.min or options.max or options.buckets) and options.custbuckets:
+        print ("Cannot specify min, max or number of buckets " 
+              "as well as customer buckets\n")
+        parser.print_usage()
+        sys.exit(1)
+
     if sys.stdin.isatty():
         # if isatty() that means it's run without anything piped into it
         parser.print_usage()
         print "for more help use --help"
         sys.exit(1)
+
     histogram(load_stream(sys.stdin, options.agg_value_key,
                           options.agg_key_value), options)
+
